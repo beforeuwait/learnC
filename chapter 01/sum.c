a1f45097cb784ca3b130f346bdfa65d502b7d7fd
王家葳 <forme.wjw@aliyun.com>
2019-05-22 17:40:51 +0800

    sum

--- chapter 01/sum.c
+++ chapter 01/sum.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int a, b ,sum;
-    a = 520;
-    b = 1314;
-    sum = a + b;
-    printf("sum is %d\n", sum);
+    int a, b ,sum;  // 定义 int 型
+    a = 520;        // 赋值
+    b = 1314;       // 赋值
+    sum = a + b;    // 表达式
+    printf("sum is %d\n", sum); //  输出
     return 0;
 }
