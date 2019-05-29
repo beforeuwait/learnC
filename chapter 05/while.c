df98a7107ae33cb0945d9396338f2c3cf7808f9e
王家葳 <forme.wjw@aliyun.com>
2019-05-29 17:10:29 +0800

    sum 1-100

--- chapter 05/while.c
+++ chapter 05/while.c
@@ -4,11 +4,25 @@
  do{} while (); 也就是无论如何都要执行一次
 */
 
-int main(){
+int main1(){
     int i = 1;
     do {
         printf("%d\n", i);
         i ++;
         }
         while (i <= 10);
+    return 0;
+}
+
+/*计算 1-100的加法*/
+
+int main(){
+    int i = 1, sum = 0;
+    do{
+        sum +=i;
+        i++;
+    }
+    while (i<=100);
+    printf("sum is:\t%d\n", sum);
+    return 0;
 }
