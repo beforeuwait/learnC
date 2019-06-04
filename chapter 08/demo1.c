807d93202b30b5fb5a3765c182dd807fda3edb5f
王家葳 <forme.wjw@aliyun.com>
2019-06-04 16:40:07 +0800

    add

--- chapter 08/demo1.c
+++ chapter 08/demo1.c
@@ -2,7 +2,7 @@
 
 int main(){
     int a = 100, b = 10;
-    int *pointer_1, *pointer_2;     //这里仅仅是定义 指针变量
+    int *pointer_1, *pointer_2;     //这里仅仅是定义 指针变量, 同时关于指向基类型, 指向是什么类型就要定义什么类型
     pointer_1 = &a;
     pointer_2 = &b;
     printf("a=%d,b=%d\n", a, b);
