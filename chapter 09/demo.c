6174f286178fdd54c24d31441f623de884611c0e
王家葳 <forme.wjw@aliyun.com>
2019-07-10 11:23:45 +0800

    ????

--- chapter 09/demo.c
+++ chapter 09/demo.c
@@ -1,16 +1,13 @@
 # include<stdio.h>
 
-struct Date{
-    int year;
-    int month;
-    int day;
-};
+int main(){
+    struct Student{
+        long int num;   // 学号
+        char name[20];  //姓名
+        char sex;       //性别
+        char addr[20];  // 地址
+    } xiaoming={201113060116, "xiaoming", 'M', "chengdu"},xiaodong={201113060117, "xiaodong", 'F', "chengdu"};
+    printf("No: %ld\t,name: %s\t,sex: %c\t,addr: %s\n", xiaodong.num, xiaodong.name, xiaodong.sex, xiaodong.addr);
 
-struct Student
-{
-    int age;
-    char height;
-    char sex;
-    char addr[30];
-    struct Date birthday;
-} xiaoming, xiaodong;
+    return 0;
+}
